7906dfe3227a492829c81e10a9ff19210b77c71f
TaslimOseni <tasleemoseni@gmail.com>
2020-11-22 14:47:01 +0100

    Coupled navigation drawer with activity

--- app/src/main/java/com/dabinu/abu/ui/activities/MainActivity.kt
+++ app/src/main/java/com/dabinu/abu/ui/activities/MainActivity.kt
@@ -1,50 +1,87 @@
 package com.dabinu.abu.ui.activities
 
 import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dabinu.abu.R
+import com.dabinu.abu.models.DrawerItem
+import com.dabinu.abu.ui.adapters.DrawerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.drawer_content.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var navController: NavController
+
+    private var isUserLoggedIn = false
+
+    private val drawerItems = mutableListOf<DrawerItem>().apply {
+        add(DrawerItem(R.drawable.ic_home, getString(R.string.menu_home), true))
+        add(DrawerItem(R.drawable.ic_exchange_rates, getString(R.string.menu_exchange_rates), false))
+        add(DrawerItem(R.drawable.ic_countries, getString(R.string.menu_countries), false))
+        add(DrawerItem(R.drawable.ic_rate_us, getString(R.string.menu_rate_us), false))
+        add(DrawerItem(R.drawable.ic_share_app, getString(R.string.menu_share_app), false))
+    }
+
+    private val drawerAdapter by lazy { DrawerAdapter(this, drawerItems) {item ->
+        drawer_layout.closeDrawers()
+
+        when(item.icon) {
+            R.drawable.ic_home -> navController.navigate(R.id.currencyConversionFragment)
+            R.drawable.ic_exchange_rates -> navController.navigate(R.id.exchangeRatesFragment)
+            R.drawable.ic_countries -> navController.navigate(R.id.countriesFragment)
+            R.drawable.ic_rate_us -> { }
+            R.drawable.ic_share_app -> { }
+        }
+    } }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        setupViews()
+        observe()
+
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
+
+    private fun setupViews() {
+
+        navController = findNavController( R.id.nav_host_fragment)
+
+        toggle =
+            ActionBarDrawerToggle(this, drawer_layout, R.string.app_name, R.string.app_name)
+        toggle.syncState()
+
+        rvDrawer.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        rvDrawer.adapter = drawerAdapter
+
+
+        btnDrawerLogout.setOnClickListener { if(isUserLoggedIn) logout() else login() }
+
+        drawerHeader.setOnClickListener { navController.navigate(R.id.profileFragment) }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+
+    private fun observe() {
+
     }
+
+
+    private fun login() {
+
+    }
+
+    private fun logout() {
+
+    }
+
+
 }
