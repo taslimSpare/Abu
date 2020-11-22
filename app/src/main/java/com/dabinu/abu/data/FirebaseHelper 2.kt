bf012af13015b1f366e1bbe2e4aadaa6dea282e4
TaslimOseni <tasleemoseni@gmail.com>
2020-11-22 13:59:56 +0100

    Created app fragments

--- app/src/main/java/com/dabinu/abu/data/FirebaseHelper 2.kt
+++
@@ -1,4 +0,0 @@
-package com.dabinu.abu.data.data
-
-class FirebaseHelper {
-}
--- app/src/main/java/com/dabinu/abu/models/Drawer 2.kt
+++
@@ -1,3 +0,0 @@
-package com.dabinu.abu.data.models
-
-class DrawerItem(val icon: Int, val title: String, var selected: Boolean = false)
